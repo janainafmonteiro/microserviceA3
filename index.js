dae565cbfca83c26dcdbd35b49325346cec771a0
Janaina <janainaferreiramonteiro@hotmail.com>
2026-05-03 21:36:39 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Servidor rodando com sucesso!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor ativo em http://localhost:${PORT}`);
+});
