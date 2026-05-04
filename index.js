68a28c72445b73bdd00936ca562ec882ce752f96
Janaina <janainaferreiramonteiro@hotmail.com>
2026-05-03 23:45:13 -0300

    conexao db

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express');
 
 const app = express();
 const PORT = 3000;
+const db = require("./src/config/db")
 
 app.use(express.json());
 
-const conectarBanco = async() =>{
+db.authenticate().then(() => {
+  console.log("conectado")
+  app.listen(PORT, function(){
+    console.log(`Servidor ativo em http://localhost:${PORT}`)
+  })
+}).catch(err => {
+  console.log("err", err)
+})
 
-}
 
 app.get('/', (req, res) => {
     res.send('Servidor rodando com sucesso!');
