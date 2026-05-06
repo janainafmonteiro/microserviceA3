9dbb735119d697bd35a965e09e053719ffaeb2a6
Janaina <janainaferreiramonteiro@hotmail.com>
2026-05-06 01:07:18 -0300

    feat: add get Evento Service

--- src/services/EventoService.js
+++ src/services/EventoService.js
@@ -1,5 +1,15 @@
 const eventoService = {
 
+  async listAllEventos(){
+    try{
+      const[rows] = await db.query('SELECT id_evento responsavelid, endereco, dataini, datafim, nome, descricao, capacidade, dispo, status FROM eventos')
+      return rows
+    }catch(error){
+      console.log('erro a pegar os eventoService ', err)
+      throw new Error('erro a pegar os evento')
+    }
+  },
+
   async addEvento(responsavelid, endereco, dataini, datafim, nome, descricao, capacidade, dispo, status){
     try{
       const query = 'INSERT INTO eventos (responsavelid, endereco, dataini, datafim, nome, descricao, capacidade, dispo, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)'
